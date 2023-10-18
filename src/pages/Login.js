31195615605e2935cdb4bc17d2a78d8a83f61ba9
saber <106769715+SabMehdi@users.noreply.github.com>
2023-10-18 23:03:07 +0200

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <input type='text' name='username' placeholder='Enter username'> </input>
+        <input type='password' name='password' placeholder='Enter password'> </input>
+        <input type='submit'/>
+    </div>
+  )
+}
+
+export default Login
