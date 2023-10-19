334baa4ac83f5002d5f03c696e320fc9cd2c0809
saber <106769715+SabMehdi@users.noreply.github.com>
2023-10-19 02:19:00 +0200

    signup,header and context tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,28 @@ import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 
 const Header = () => {
-  let {name}=useContext(AuthContext)
+    let { user } = useContext(AuthContext)
     return (
-    <div>
-        <Link to="/">Home</Link>
-        <span> | </span>
-        <Link to="/login">Login</Link>
-        <span> | </span>
-        <Link to="/register">Register</Link>
-    </div>
-  )
+        <div>
+            <Link to="/">Home</Link>
+            <span> | </span>
+            {
+                user ? (
+                    <span>
+                        <span> Welcome, {user.username}!</span>
+                        <span> | </span>
+                        <span> Logout</span>
+                    </span>
+
+                ) :
+                    <span>
+                        <Link to="/login">Login</Link>
+                        <span> | </span>
+                        <Link to="/signup">Signup</Link>
+                    </span>
+            }
+        </div>
+    )
 }
 
 export default Header
